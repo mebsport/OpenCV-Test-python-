be753dc238e688e2cc131084a1fca7876812348f
mebsport <56307645+mebsport@users.noreply.github.com>
2023-11-29 19:14:49 -0500

    Color Changes + Comments + Cleaning
    - updated color values for the red and blue
    - added variables for the white mask and tile mask
    - added comments to explain code
    - added more images that save for easier testing
    - cleaned up code

--- BlockTest.py
+++ BlockTest.py
@@ -1,35 +1,47 @@
 
 import numpy as np
 import cv2
-import random
-import numpy as np
-import cv2
-import random
 
-#Load an image
-img = cv2.imread('Images/BLUE_CENTER.jpg')
+# Load an image and save to a variable named img
+img = cv2.imread('Images/RED_CENTER.jpg')
 
+# check to make sure the immage exists so no errors occur
 if img is None:
     print('Failed to load image file:')
     sys.exit(1)
 
-#Variables
-blue = np.uint8([[[36,52,93]]])
-red = np.uint8([[[125,29,14]]])
+# create a grayscale copy of the image
+grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-lowerBlue = np.array([[[  0, 50,  50]]])
-highBlue = np.array([[[  28, 255,  255]]])
-lowerRed = np.array([[[94, 50, 50]]])
-highRed = np.array([[[136, 255, 2555]]])
+#Variables
+blue_BAD = np.uint8([[[36,52,93]]])
+red_BAD = np.uint8([[[125,29,14]]])
+
+
+lowerBlue = np.array([[[  105, 50,  50]]])
+highBlue = np.array([[[  125, 255, 255]]])
+
+lowerRed = np.array([[[-5, 50, 50]]])
+highRed = np.array([[[15, 255, 255]]])
+
+lowTile = np.array([[[  110,  110,  110]]])
+highTile = np.array([[[  160, 160, 160]]])
+
+lowWhite = np.array([[[  235,  235,  235]]])
+highWhite = np.array([[[  255, 255, 255]]])
 
 # Convert image to HSV
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-# Threshold the HSV image to get only blue colors
-mask = cv2.inRange(hsv, lowerBlue, highBlue)
+# use a mask on the HSV image to get only wanted colors
+mask = cv2.inRange(hsv, lowWhite, highWhite)
+bwMask = cv2.inRange(grayImg, lowTile, highTile)
 
-# Save the image to a file
+# Save the images to a file
 cv2.imwrite('output.jpg', mask)
+cv2.imwrite('bwMask.jpg', bwMask)
+cv2.imwrite('hsv.jpg', hsv)
+cv2.imwrite('bw.jpg', grayImg)
 
 # Convert blue to HSV
 # hsv_blue = cv2.cvtColor(red, cv2.COLOR_BGR2HSV)
