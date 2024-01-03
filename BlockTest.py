1dd94171118921ec99f4bf059c820eb34d041756
mebsport <56307645+mebsport@users.noreply.github.com>
2024-01-02 20:29:21 -0500

    Testing with images from robot

--- BlockTest.py
+++ BlockTest.py
@@ -1,9 +1,10 @@
 
 import numpy as np
 import cv2
+import cv2
 
 # Load an image and save to a variable named img
-img = cv2.imread('Images/RED_CENTER.jpg')
+img = cv2.imread('Images/FROM_ROBOT_1-2-24/REDRIGHT.jpg')
 
 # check to make sure the immage exists so no errors occur
 if img is None:
@@ -34,7 +35,7 @@ highWhite = np.array([[[  255, 255, 255]]])
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
 # use a mask on the HSV image to get only wanted colors
-mask = cv2.inRange(hsv, lowWhite, highWhite)
+mask = cv2.inRange(hsv, lowerBlue, highBlue)
 bwMask = cv2.inRange(grayImg, lowTile, highTile)
 
 # Save the images to a file
@@ -45,5 +46,39 @@ cv2.imwrite('bw.jpg', grayImg)
 
 # Convert blue to HSV
 # hsv_blue = cv2.cvtColor(red, cv2.COLOR_BGR2HSV)
+
+
+def crop_image(image, x, y, width, height):
+    cropped_image = image[y:y+height, x:x+width]
+    return cropped_image
+
+# img = cv2.imread('Images/FROM_ROBOT_1-2-24/REDRIGHT.jpg')
+img = mask
+
+# Left
+cropped_img = crop_image(img, 0, 427, 357, 293)
+cv2.imwrite('left_cropped_image.jpg', cropped_img)
+leftCount = np.count_nonzero(cropped_img)
+
+#Right
+cropped_img = crop_image(img, 898, 427, 357, 293)
+cv2.imwrite('right_cropped_image.jpg', cropped_img)
+rightCount = np.count_nonzero(cropped_img)
+
+
+#Center
+cropped_img = crop_image(img, 386, 427, 462, 165)
+cv2.imwrite('center_cropped_image.jpg', cropped_img)
+midCount = np.count_nonzero(cropped_img)
+
+highNum = max(leftCount,rightCount,midCount)
+
+if(highNum == leftCount):
+    print("Left")
+elif(highNum == rightCount):
+    print("Right")
+else:
+    print("Center")
+
 
 
